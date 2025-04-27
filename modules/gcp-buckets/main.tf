e3136a660c67095a8d1c741f7a984951ffe60e12
Aurimas L <kovas6@gmail.com>
2025-04-27 11:57:45 +0300

    ok

--- modules/gcp-buckets/main.tf
+++ modules/gcp-buckets/main.tf
@@ -5,11 +5,11 @@ terraform {
   }
 }
 
-resource "google_storage_bucket" "buckets" {
-  for_each = toset(var.bucket_names)
-
-  name     = each.key
-  location = "US"
-  force_destroy = true
-  uniform_bucket_level_access = true
-}
+#resource "google_storage_bucket" "buckets" {
+#  for_each = toset(var.bucket_names)
+#
+#  name     = each.key
+#  location = "US"
+#  force_destroy = true
+#  uniform_bucket_level_access = true
+#}
