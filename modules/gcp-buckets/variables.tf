facc16588549afea72ce2f5b03a7d3fd19c2ad3e
Aurimas L <kovas6@gmail.com>
2025-04-27 14:30:01 +0300

    sdsds

--- modules/gcp-buckets/variables.tf
+++ modules/gcp-buckets/variables.tf
@@ -4,3 +4,10 @@ variable "buckets" {
     location = string
   }))
 }
+
+resource "google_storage_bucket" "buckets" {
+  for_each = var.buckets
+
+  name     = each.key
+  location = each.value.location
+}
