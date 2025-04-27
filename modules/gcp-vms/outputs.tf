37c93bf832f6d58de1ee8e43c499186e6fc6fd54
Aurimas L <kovas6@gmail.com>
2025-04-27 17:56:46 +0300

    sss

--- modules/gcp-vms/outputs.tf
+++ modules/gcp-vms/outputs.tf
@@ -1,3 +1,7 @@
 output "vm_ips" {
-  value = google_compute_instance.vms[*].network_interface[0].access_config[0].nat_ip
+  description = "Public IP addresses of VMs"
+  value = {
+    for vm_key, vm_resource in google_compute_instance.vms :
+    vm_key => vm_resource.network_interface[0].access_config[0].nat_ip
+  }
 }
